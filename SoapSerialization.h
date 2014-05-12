2b8666f6146c7db1cfae8def5c056d647d08ad87
Anders Borum <palmin@users.noreply.github.com>
2014-05-12 10:49:28 +0200

    removed example now present in README.md

--- SoapSerialization.h
+++ SoapSerialization.h
@@ -3,25 +3,6 @@
 //     Lightweight methods for encoding and decoding SOAP 1.1 envelopes
 //     with support for creating a NSURLRequest to perform SOAP requests.
 //
-//  A small example testing a SOAP request to service echoing input.
-//    NSString* endpoint = @"http://www.SoapClient.com/xml/soapresponder.wsdl";
-//    NSDictionary* object = @{@"bstrParam1": @"Hello",
-//                             @"bstrParam2": @"World"};
-//    NSURLRequest* request = [SoapSerialization requestForSoapEndpoint:[NSURL URLWithString:endpoint]
-//                                                               object:object name:@"Method1"
-//                                                               action:@"SoapObject" namespace:endpoint];
-//
-//    [NSURLConnection sendAsynchronousRequest:request queue:[NSOperationQueue mainQueue]
-//                           completionHandler:^(NSURLResponse* response, NSData* data, NSError* error) {
-//                               NSError* soapError = nil;
-//                               NSDictionary* dictionary = [SoapSerialization SOAPObjectWithData:data error:&soapError];
-//
-//                               // Outputs: { bstrReturn = "Your input parameters are Hello and World"; }
-//                               NSLog(@"%@", dictionary != nil ? dictionary : soapError);
-//                           }];
-//
-//
-//
 // Copyright (c) 2014 Anders Borum
 //
 // Permission is hereby granted, free of charge, to any person obtaining a copy
